2b5016c149d1efccb7916ad13dc9e9da4baeb919
--add <sumit.pradhan@digimantra.com>
2023-01-11 18:40:08 +0530

    Final Commit

--- VITAMIN D/VITAMIN_DEMO/app/views.py
+++ VITAMIN D/VITAMIN_DEMO/app/views.py
@@ -11,37 +11,29 @@ from .serializers import *
 
 @api_view(['GET'])
 def view_data(request):
-    zip_code = request.GET['zip']
-    try:
-        # data = SunshineAvailability.objects.all()
-        # serializer = SunshineAvailabilitySerializer(data=data, many=True, context={'request': request})
-        # if serializer.is_valid():
-        #     return Response({
-        #                 'status': True,
-        #                 'message': "Fetched Successfully!!",
-        #                 'zones': serializer.data
-        #         })
-        data = []
-        latitude = ZipCodes.objects.get(zip_code=zip_code).latitude
-        zone_data = Zones.objects.filter(LatitudeMin__lte=latitude, LatitudeMax__gte=latitude)
-        serializer = ZoneSerializer(data=zone_data, many=True, context={'request': request})
-        if serializer.is_valid():
+        zip_code = request.GET['zip']
+        try:
+            latitude = ZipCodes.objects.get(zip_code=zip_code).latitude
+            zone_data = Zones.objects.filter(LatitudeMin__lte=latitude, LatitudeMax__gte=latitude)
+            print(zone_data)
+            serializer = ZoneSerializer(data=zone_data, many=True, context={'request': request})
 
-            for i in zone_data:
-                zone_data
-                SunshineAvailability.objects.filter(ZoneId=zoneId).value_list(flat=True)
-                data.append()
-
+            result = SunshineAvailability.objects.filter(zoneId=4).value_list(flat=True)
+            print(result)
+            serializer = SunshineAvailabilitySerializer(data=result, many=True, context={'request': request})
+            if serializer.is_valid():
                 return Response({
-                        'status': True,
-                        'message': "Fetched Successfully!!",
-                        'data': data
-                })
-    except Exception as e:
-        print(e)
+                    'status': True,
+                    'message': "Fetched Successfully!!",
+                    'data': serializer.data
+                 })
+        except Exception as e:
+            print(e)
 
-    return Response({
+        return Response({
         'status': True,
         'message': "Fetched Successfully!!",
-        'data': data
+
     })
+
+
